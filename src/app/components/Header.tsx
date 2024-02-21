3b26aea9c86624002d1b7b9a121c8244a624963a
Maicon Andreis <maiconrandreis@gmail.com>
2024-02-20 23:47:24 -0300

    Modificação de Componentes e separação de dentro do Heade, Adição de novos Componentes. Inciando função de testes e aplicando ao useScroll

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,51 +1,31 @@
 import { w } from "@vidstack/react/dist/types/vidstack.js";
 import { headers } from "next/dist/client/components/headers";
 'use client';
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Logo } from "./Logo";
 import Link from "next/link";
 import { NavLinks } from "./NavLinks";
+import { UserProfile } from "./UserProfile";
+import { useScroll } from "../hooks/useScroll";
+
 
 export default function Header () {
-    const [ isScrolled, setIsScrolled] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 0) {
-                setIsScrolled(true);
-            } else {
-                setIsScrolled(false);
-            }
-        };
-
-        window.addEventListener("scroll", handleScroll);
-
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
+    const isScrolled = useScroll();         
 
     return (
-        <header className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-10 lg:py-6 `}> 
+        <header className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between 
+        bg-gradient-to-t from-transparent to-black p-2 px-4 transition-all lg:px-16 lg:py-6 `}> 
 
             <div className=" flex items-center space-x-2 md:space-x-8">
                 <Link href='/'>
                     <Logo />                
                 </Link>
                 <NavLinks />
-            </div>
+            </div>            
+                           
+            <UserProfile />           
             
-            <div className=" flex items-center space-x-2 md:space-x-8">
-                
-                <p className=" hidden cursor-not-allowed lg:inline">Kids</p>
-                <img src="/profile.png" alt="profile" className=" cursor-pointer rounded"/>
-
-            </div>
-            
-        </header>
-        
+        </header>        
 
     )
-
-
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,19 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "./components/Header";
+import { MovieRow } from "./components/MovieRow";
+import { Banner } from "./components/Banner";
 
 const inter = Inter({ subsets: ["latin"] });
-
 
 export default function Home() {
   return (
     <div className="relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]">
       <Header />
-      <main className="relative pb-24 pl-4 lg:pl-16">
-        <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">        
-          <div className="absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black">
-            <Image src='/the-witcher2.jpg' alt="The Witcher" fill={true} className=" h-[65vh] object-cover object-top lg:h-[95vh] "/>
-          </div>          
-
-          <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
-            The Witcher            
-          </h1>
-          <p className=" max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl">
-          Geralt of Rivia, a monster-hunting mutant, sets out in search of his destiny in a 
-          turbulent world, where people can be more wicked than wild creatures.
-          </p>
-        </div>
-
-        <div className="flex space-x-3">
-          <button className=" md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5"> 
-            Play 
-          </button>
-          <button className=" md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-gray-950 transition hover:opacity-75 md:px-8 md:py-2.5"> 
-            More Info 
-          </button>
-
-        </div>
+      <main className="relative pb-24 pl-4 lg:pl-16">        
+        <Banner />
+        <MovieRow sectionTitle="Tredding Now" />
+        <MovieRow sectionTitle="Top Rated" />
+        <MovieRow sectionTitle="Action Movies" />                       
 
       </main>
     </div>
